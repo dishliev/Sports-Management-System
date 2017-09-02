cc3985b0e42ecfeaaf4917abbd8b89b92f131845
Kiril Dishliev <kiril.dishliev1@gmail.com>
2017-09-02 18:44:37 +0300

    client list - DBQuery

--- Sports Management System/Sports Management System/Clients/Clients.aspx.cs
+++ Sports Management System/Sports Management System/Clients/Clients.aspx.cs
@@ -1,71 +1,19 @@
-﻿using Sports_Management_System.Models;
-using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Web;
-using System.Web.UI;
-using System.Web.UI.WebControls;
-
-namespace Sports_Management_System.Clients
+﻿namespace Sports_Management_System.Clients
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Configuration;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Web.UI.WebControls;
     public partial class Clients : System.Web.UI.Page
     {
-        private static string sqlConn = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+      
         protected void Page_Load(object sender, EventArgs e)
         {
-            using (SqlConnection con = new SqlConnection(sqlConn))
-            {
-                string query = "select * from clients";
-                con.Open();
-                using (SqlCommand command = new SqlCommand(query, con))
-                {
-                    SqlDataReader reader = command.ExecuteReader();
-
-                    List<Dictionary<string, string>> rows = new List<Dictionary<string, string>>();
-                    Dictionary<string, string> column;
-                    DataTable dt = new DataTable();
-
-                    while (reader.Read())
-                    {
-                        column = new Dictionary<string, string>();
-
-                        column["clientId"] = reader["clientId"].ToString();
-                        column["name"] = reader["name"].ToString();
-                        column["surname"] = reader["surname"].ToString();
-                        column["age"] = reader["age"].ToString();
-                        column["email"] = reader["email"].ToString();
-                        column["address"] = reader["address"].ToString();
-                        column["phone"] = reader["phone"].ToString();
-                       
-                        rows.Add(column);
-                    }
-
-                    con.Close();
-
-                    dt.Columns.Add("clientId");
-                    dt.Columns.Add("name");
-                    dt.Columns.Add("surname");
-                    dt.Columns.Add("age");
-                    dt.Columns.Add("email");
-                    dt.Columns.Add("address");
-                    dt.Columns.Add("phone");
-
-                    foreach (Dictionary<string, string> contacts in rows)
-                    {
-                        dt.Rows.Add(contacts["clientId"],contacts["name"],
-                                    contacts["surname"],contacts["age"],
-                                    contacts["email"], contacts["address"], 
-                                    contacts["phone"]);
-                    }
-                    repCustomers.DataSource = dt;
-                    repCustomers.DataBind();
-
-                }
-
-            }
+           DataTable dt = DBQuery.Clients();
+           repCustomers.DataSource = dt;
+           repCustomers.DataBind();       
         }
 
         protected void client_ItemCommand(object source, RepeaterCommandEventArgs e)
--- Sports Management System/Sports Management System/DBQuery.cs
+++ Sports Management System/Sports Management System/DBQuery.cs
@@ -3,10 +3,60 @@
     using Sports_Management_System.Models;
     using System.Collections.Generic;
     using System.Configuration;
+    using System.Data;
     using System.Data.SqlClient;
     public class DBQuery
     {
-     
+        private static string sqlConn = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+        public static DataTable Clients ()
+        {
+            using (SqlConnection con = new SqlConnection(sqlConn))
+            {
+                string query = "select * from clients";
+                con.Open();
+                using (SqlCommand command = new SqlCommand(query, con))
+                {
+                    SqlDataReader reader = command.ExecuteReader();
 
+                    List<Dictionary<string, string>> rows = new List<Dictionary<string, string>>();
+                    Dictionary<string, string> column;
+                    DataTable dt = new DataTable();
+
+                    while (reader.Read())
+                    {
+                        column = new Dictionary<string, string>();
+
+                        column["clientId"] = reader["clientId"].ToString();
+                        column["name"] = reader["name"].ToString();
+                        column["surname"] = reader["surname"].ToString();
+                        column["age"] = reader["age"].ToString();
+                        column["email"] = reader["email"].ToString();
+                        column["address"] = reader["address"].ToString();
+                        column["phone"] = reader["phone"].ToString();
+
+                        rows.Add(column);
+                    }
+
+                    con.Close();
+
+                    dt.Columns.Add("clientId");
+                    dt.Columns.Add("name");
+                    dt.Columns.Add("surname");
+                    dt.Columns.Add("age");
+                    dt.Columns.Add("email");
+                    dt.Columns.Add("address");
+                    dt.Columns.Add("phone");
+
+                    foreach (Dictionary<string, string> contacts in rows)
+                    {
+                        dt.Rows.Add(contacts["clientId"], contacts["name"],
+                                    contacts["surname"], contacts["age"],
+                                    contacts["email"], contacts["address"],
+                                    contacts["phone"]);
+                    }
+                    return dt;                 
+                }
+            }
+        }
     }
 }
